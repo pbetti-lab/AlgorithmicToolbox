78ecc2ddf41b1045ff4c169db3f5541e6aee6806
Paolo Betti <pbetti@hotmail.it>
2023-03-18 08:27:15 +0100

    [Solution] - This is the first commit with the backbone of the solution. What's better than an Hello Word to start a new project.

---
+++ PbettiLab.AlgorithmicToolbox.Console/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace PbettiLab.AlgorithmicToolbox.Console
+{
+	public static class Program
+	{
+		static void Main(string[] args)
+		{
+			System.Console.WriteLine("Hello, World!");
+		}
+	}
+}
---
+++ Tests/PbettiLab.AlgorithmicToolbox.LogicTests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace PbettiLab.AlgorithmicToolbox.LogicTests
+{
+	[TestClass]
+	public class UnitTest1
+	{
+		[TestMethod]
+		public void TestMethod1()
+		{
+		}
+	}
+}
