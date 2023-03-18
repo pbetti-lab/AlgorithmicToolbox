b8ff1c735d4d3d8caac863d96b7f401a6ec2bfe8
Paolo Betti <pbetti@hotmail.it>
2023-03-18 09:15:25 +0100

    [PbettiLab.AlgorithmicToolbox.Logic.UnitTests] - Fixing project name convention error

---
+++ PbettiLab.AlgorithmicToolbox.Logic.UnitTests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace PbettiLab.AlgorithmicToolbox.Logic.UnitTests
+{
+	[TestClass]
+	public class UnitTest1
+	{
+		[TestMethod]
+		public void TestMethod1()
+		{
+		}
+	}
+}
