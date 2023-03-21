7e37cf79c1e4af1fffb05c90110bfa9e79d230cc
Paolo Betti <pbetti@hotmail.it>
2023-03-21 17:38:41 +0100

    [PbettiLab.AlgorithmicToolbox.Logic] - add linear search logic

---
+++ PbettiLab.AlgorithmicToolbox.Logic/Searching/Interfaces/IArraySearch.cs
@@ -0,0 +1,19 @@
+﻿namespace PbettiLab.AlgorithmicToolbox.Logic.Searching.Interfaces
+{
+	public interface IArraySearch
+	{
+		public bool Find(byte[] elements, byte value);
+
+		public bool Find(int[] elements, int value);
+
+		public bool Find(long[] elements, long value);
+
+		public bool Find(float[] elements, float value);
+
+		public bool Find(double[] elements, double value);
+
+		public bool Find(decimal[] elements, decimal value);
+
+		public bool Find(string[] elements, string value);
+	}
+}
---
+++ PbettiLab.AlgorithmicToolbox.Logic/Searching/Interfaces/IEnumerableSearch.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+
+namespace PbettiLab.AlgorithmicToolbox.Logic.Searching.Interfaces
+{
+	public interface IEnumerableSearch
+	{
+		public bool Find(IEnumerable<byte> elements, byte value);
+
+		public bool Find(IEnumerable<int> elements, int value);
+
+		public bool Find(IEnumerable<long> elements, long value);
+
+		public bool Find(IEnumerable<float> elements, float value);
+
+		public bool Find(IEnumerable<double> elements, double value);
+
+		public bool Find(IEnumerable<decimal> elements, decimal value);
+
+		public bool Find(IEnumerable<string> elements, string value);
+	}
+}
