baf22c8d8b426a38271a3563d5a64452c6b165c4
Paolo Betti <pbetti@hotmail.it>
2023-06-18 15:29:03 +0200

    [PbettiLab.AlgorithmicToolbox.Logic, PbettiLab.AlgorithmicToolbox.Logic.UnitTests] - Add quick sort to algoritm toolbox

---
+++ PbettiLab.AlgorithmicToolbox.Logic/Sort/Algorithms/QuickSortRecursive.cs
@@ -0,0 +1,115 @@
+﻿using PbettiLab.AlgorithmicToolbox.Logic.Sort.Interfaces;
+using System;
+
+namespace PbettiLab.AlgorithmicToolbox.Logic.Sort.Algorithms
+{
+	public class QuickSortRecursive : ISortable
+	{
+		private readonly Random _randomizer;
+
+		public QuickSortRecursive() 
+		{
+			_randomizer = new Random();
+		}
+
+		/// <summary>
+		/// Sort the elements by ascending order.
+		/// </summary>
+		/// <param name="elements">The elements to sort.</param>
+		/// <exception cref="ArgumentNullException">If the elements array is null.</exception>
+		public void Order(int[] elements)
+		{
+			if (elements is null)
+			{
+				throw new ArgumentNullException(nameof(elements), "Input variable elements cannot be null");
+			}
+
+			if (elements.Length <= 1)
+			{
+				return;
+			}
+
+			Sort(elements, 0, elements.Length - 1);
+		}
+
+		public void OrderByDescending(int[] elements)
+		{
+			Order(elements);
+			ReverseOrder(elements);
+		}
+
+		private void Sort(int[] elements, int leftIndex, int rightIndex)
+		{
+			if (leftIndex >= rightIndex)
+				return;
+
+			int startingPivotIndex = GetRandomPivotIndex(leftIndex, rightIndex);
+			int sortedPivotIndex = ElementsPartition(elements, startingPivotIndex, leftIndex, rightIndex);
+
+			Sort(elements, leftIndex, sortedPivotIndex - 1);
+			Sort(elements, sortedPivotIndex + 1, rightIndex);
+		}
+
+		private int GetRandomPivotIndex(int leftIndex, int rightIndex)
+		{
+			return _randomizer.Next(leftIndex, rightIndex + 1);
+		}
+
+		private int ElementsPartition(int[] elements, int startingPivotIndex, int leftIndex, int rightIndex)
+		{
+			PutPivotElementInFirstPosition(elements, startingPivotIndex, leftIndex);
+
+			startingPivotIndex = leftIndex;
+			int sortedPivotIndex = leftIndex;
+
+			for (int i = leftIndex + 1; i <= rightIndex; i++)
+			{
+				if (elements[i] <= elements[startingPivotIndex])
+				{
+					if (sortedPivotIndex + 1 == i)
+					{ 
+						sortedPivotIndex++;
+					}
+					else 
+					{
+						sortedPivotIndex++;
+						SwapValues(elements, i, sortedPivotIndex);
+					}
+				}
+			}
+
+			PutPivotElementInFinalSortedPosition(elements, startingPivotIndex, sortedPivotIndex);
+
+			return sortedPivotIndex;
+		}
+
+		private void PutPivotElementInFinalSortedPosition(int[] elements, int startingPivotIndex, int sortedPivotIndex)
+		{
+			if (startingPivotIndex != sortedPivotIndex)
+			{ 
+				SwapValues(elements, startingPivotIndex, sortedPivotIndex);
+			}
+		}
+
+		private void PutPivotElementInFirstPosition(int[] elements, int startingPivotIndex, int leftIndex)
+		{
+			if (startingPivotIndex != leftIndex)
+			{ 
+				SwapValues(elements, startingPivotIndex, leftIndex);
+			}
+		}
+
+		private void SwapValues(int[] elements, int leftPosition, int rightPosition)
+		{
+			(elements[leftPosition], elements[rightPosition]) = (elements[rightPosition], elements[leftPosition]);
+		}
+
+		private void ReverseOrder(int[] elements)
+		{
+			for (int i = 0; i < (elements.Length / 2); i++)
+			{
+				SwapValues(elements, elements.Length - 1 - i, i);
+			}
+		}
+	}
+}
---
+++ Tests/PbettiLab.AlgorithmicToolbox.Logic.UnitTests/Sort/Algorithms/QuickSortIterativeTests.cs
@@ -0,0 +1,311 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PbettiLab.AlgorithmicToolbox.Logic.Sort.Algorithms;
+using PbettiLab.AlgorithmicToolbox.Logic.Sort.Interfaces;
+using System;
+
+namespace PbettiLab.AlgorithmicToolbox.Logic.UnitTests.Sort.Algorithms
+{
+	[TestClass]
+	public class QuickSortIterativeTests
+	{
+		[TestMethod]
+		public void Order_InputElementsIsNull_ThrowArgumentNullException()
+		{
+			int[] elements = null;
+			ISortable quickSort = new QuickSortIterative();
+
+			Assert.ThrowsException<ArgumentNullException>(() => quickSort.Order(elements));
+		}
+
+		[TestMethod]
+		public void Order_InputElementsIsEmpty_ElementsRemainsTheSame()
+		{
+			int[] elements = new int[] { };
+			ISortable quickSort = new QuickSortIterative();
+
+			quickSort.Order(elements);
+
+			Assert.AreEqual(0, elements.Length);
+		}
+
+		[TestMethod]
+		public void Order_InputElementsContainsOnlyOneValue_ElementsRemainsTheSame()
+		{
+			const int ELEMENT_VALUE = 6;
+			int[] elements = new int[] { ELEMENT_VALUE };
+			ISortable quickSort = new QuickSortIterative();
+
+			quickSort.Order(elements);
+
+			Assert.AreEqual(1, elements.Length);
+			Assert.AreEqual(ELEMENT_VALUE, elements[0]);
+		}
+
+		[TestMethod]
+		public void Order_SortElementsTest1_ElementsAreOrdered()
+		{
+			ISortable quickSort = new QuickSortIterative();
+
+			int[] elements = new int[] { 6, 5 };
+			quickSort.Order(elements);
+
+			Assert.AreEqual(2, elements.Length);
+			for (int i = 1; i < elements.Length - 1; i++)
+				Assert.IsTrue(elements[i - 1] <= elements[i]);
+		}
+
+		[TestMethod]
+		public void Order_SortElementsTest2_ElementsAreOrdered()
+		{
+			ISortable quickSort = new QuickSortIterative();
+
+			int[] elements = new int[] { 5, 5 };
+			quickSort.Order(elements);
+
+			Assert.AreEqual(2, elements.Length);
+			for (int i = 1; i < elements.Length - 1; i++)
+				Assert.IsTrue(elements[i - 1] <= elements[i]);
+		}
+
+		[TestMethod]
+		public void Order_SortElementsTest3_ElementsAreOrdered()
+		{
+			ISortable quickSort = new QuickSortIterative();
+
+			int[] elements = new int[] { 5, 6 };
+			quickSort.Order(elements);
+
+			Assert.AreEqual(2, elements.Length);
+			for (int i = 1; i < elements.Length - 1; i++)
+				Assert.IsTrue(elements[i - 1] <= elements[i]);
+		}
+
+		[TestMethod]
+		public void Order_SortElementsTest4_ElementsAreOrdered()
+		{
+			ISortable quickSort = new QuickSortIterative();
+
+			int[] elements = new int[] { 6, 5, 4 };
+			quickSort.Order(elements);
+
+			Assert.AreEqual(3, elements.Length);
+			for (int i = 1; i < elements.Length - 1; i++)
+				Assert.IsTrue(elements[i - 1] <= elements[i]);
+		}
+
+		[TestMethod]
+		public void Order_SortElementsTest5_ElementsAreOrdered()
+		{
+			ISortable quickSort = new QuickSortIterative();
+
+			int[] elements = new int[] { 4, 5, 6 };
+			quickSort.Order(elements);
+
+			Assert.AreEqual(3, elements.Length);
+			for (int i = 1; i < elements.Length - 1; i++)
+				Assert.IsTrue(elements[i - 1] <= elements[i]);
+		}
+
+		[TestMethod]
+		public void Order_SortElementsTest6_ElementsAreOrdered()
+		{
+			ISortable quickSort = new QuickSortIterative();
+
+			int[] elements = new int[] { 5, 4, 6 };
+			quickSort.Order(elements);
+
+			Assert.AreEqual(3, elements.Length);
+			for (int i = 1; i < elements.Length - 1; i++)
+				Assert.IsTrue(elements[i - 1] <= elements[i]);
+		}
+
+		[TestMethod]
+		public void Order_SortElementsTest7_ElementsAreOrdered()
+		{
+			ISortable quickSort = new QuickSortIterative();
+
+			int[] elements = new int[] { 5, 6, 4 };
+			quickSort.Order(elements);
+
+			Assert.AreEqual(3, elements.Length);
+			for (int i = 1; i < elements.Length - 1; i++)
+				Assert.IsTrue(elements[i - 1] <= elements[i]);
+		}
+
+		[TestMethod]
+		public void Order_SortElementsTest8_ElementsAreOrdered()
+		{
+			ISortable quickSort = new QuickSortIterative();
+
+			int[] elements = new int[] { 12, 5, 9, 4, 5, 8, 115, 2, 3, 7, 4 };
+			quickSort.Order(elements);
+
+			Assert.AreEqual(11, elements.Length);
+			for (int i = 1; i < elements.Length - 1; i++)
+				Assert.IsTrue(elements[i - 1] <= elements[i]);
+		}
+
+		[TestMethod]
+		public void Order_SortElementsTest9_ElementsAreOrdered()
+		{
+			ISortable quickSort = new QuickSortIterative();
+
+			int[] elements = new int[] { 8, 7, 6, 5, 4, 3, 2, 1 };
+			quickSort.Order(elements);
+
+			Assert.AreEqual(8, elements.Length);
+			for (int i = 1; i < elements.Length - 1; i++)
+				Assert.IsTrue(elements[i - 1] <= elements[i]);
+		}
+
+		[TestMethod]
+		public void OrderByDescending_InputElementsIsNull_ThrowArgumentNullException()
+		{
+			int[] elements = null;
+			ISortable quickSort = new QuickSortIterative();
+
+			Assert.ThrowsException<ArgumentNullException>(() => quickSort.OrderByDescending(elements));
+		}
+
+		[TestMethod]
+		public void OrderByDescending_InputElementsIsEmpty_ElementsRemainsTheSame()
+		{
+			int[] elements = new int[] { };
+			ISortable quickSort = new QuickSortIterative();
+
+			quickSort.OrderByDescending(elements);
+
+			Assert.AreEqual(0, elements.Length);
+		}
+
+		[TestMethod]
+		public void OrderByDescending_InputElementsContainsOnlyOneValue_ElementsRemainsTheSame()
+		{
+			const int ELEMENT_VALUE = 6;
+			int[] elements = new int[] { ELEMENT_VALUE };
+			ISortable quickSort = new QuickSortIterative();
+
+			quickSort.OrderByDescending(elements);
+
+			Assert.AreEqual(1, elements.Length);
+			Assert.AreEqual(ELEMENT_VALUE, elements[0]);
+		}
+
+		[TestMethod]
+		public void OrderByDescending_SortElementsTest1_ElementsAreOrdered()
+		{
+			ISortable quickSort = new QuickSortIterative();
+
+			int[] elements = new int[] { 6, 5 };
+			quickSort.OrderByDescending(elements);
+
+			Assert.AreEqual(2, elements.Length);
+			for (int i = 1; i < elements.Length - 1; i++)
+				Assert.IsTrue(elements[i - 1] >= elements[i]);
+		}
+
+		[TestMethod]
+		public void OrderByDescending_SortElementsTest2_ElementsAreOrdered()
+		{
+			ISortable quickSort = new QuickSortIterative();
+
+			int[] elements = new int[] { 5, 5 };
+			quickSort.OrderByDescending(elements);
+
+			Assert.AreEqual(2, elements.Length);
+			for (int i = 1; i < elements.Length - 1; i++)
+				Assert.IsTrue(elements[i - 1] >= elements[i]);
+		}
+
+		[TestMethod]
+		public void OrderByDescending_SortElementsTest3_ElementsAreOrdered()
+		{
+			ISortable quickSort = new QuickSortIterative();
+
+			int[] elements = new int[] { 5, 6 };
+			quickSort.OrderByDescending(elements);
+
+			Assert.AreEqual(2, elements.Length);
+			for (int i = 1; i < elements.Length - 1; i++)
+				Assert.IsTrue(elements[i - 1] >= elements[i]);
+		}
+
+		[TestMethod]
+		public void OrderByDescending_SortElementsTest4_ElementsAreOrdered()
+		{
+			ISortable quickSort = new QuickSortIterative();
+
+			int[] elements = new int[] { 6, 5, 4 };
+			quickSort.OrderByDescending(elements);
+
+			Assert.AreEqual(3, elements.Length);
+			for (int i = 1; i < elements.Length - 1; i++)
+				Assert.IsTrue(elements[i - 1] >= elements[i]);
+		}
+
+		[TestMethod]
+		public void OrderByDescending_SortElementsTest5_ElementsAreOrdered()
+		{
+			ISortable quickSort = new QuickSortIterative();
+
+			int[] elements = new int[] { 4, 5, 6 };
+			quickSort.OrderByDescending(elements);
+
+			Assert.AreEqual(3, elements.Length);
+			for (int i = 1; i < elements.Length - 1; i++)
+				Assert.IsTrue(elements[i - 1] >= elements[i]);
+		}
+
+		[TestMethod]
+		public void OrderByDescending_SortElementsTest6_ElementsAreOrdered()
+		{
+			ISortable quickSort = new QuickSortIterative();
+
+			int[] elements = new int[] { 5, 4, 6 };
+			quickSort.OrderByDescending(elements);
+
+			Assert.AreEqual(3, elements.Length);
+			for (int i = 1; i < elements.Length - 1; i++)
+				Assert.IsTrue(elements[i - 1] >= elements[i]);
+		}
+
+		[TestMethod]
+		public void OrderByDescending_SortElementsTest7_ElementsAreOrdered()
+		{
+			ISortable quickSort = new QuickSortIterative();
+
+			int[] elements = new int[] { 5, 6, 4 };
+			quickSort.OrderByDescending(elements);
+
+			Assert.AreEqual(3, elements.Length);
+			for (int i = 1; i < elements.Length - 1; i++)
+				Assert.IsTrue(elements[i - 1] >= elements[i]);
+		}
+
+		[TestMethod]
+		public void OrderByDescending_SortElementsTest8_ElementsAreOrdered()
+		{
+			ISortable quickSort = new QuickSortIterative();
+
+			int[] elements = new int[] { 12, 5, 9, 4, 5, 8, 115, 2, 3, 7, 4 };
+			quickSort.OrderByDescending(elements);
+
+			Assert.AreEqual(11, elements.Length);
+			for (int i = 1; i < elements.Length - 1; i++)
+				Assert.IsTrue(elements[i - 1] >= elements[i]);
+		}
+
+		[TestMethod]
+		public void OrderByDescending_SortElementsTest9_ElementsAreOrdered()
+		{
+			ISortable quickSort = new QuickSortIterative();
+
+			int[] elements = new int[] { 8, 7, 6, 5, 4, 3, 2, 1 }; 
+			quickSort.OrderByDescending(elements);
+
+			Assert.AreEqual(8, elements.Length);
+			for (int i = 1; i < elements.Length - 1; i++)
+				Assert.IsTrue(elements[i - 1] >= elements[i]);
+		}
+	}
+}
